894d0693e4d62362be12cfac06ca0eb85591655c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-01-20 21:54:56 +0300

    starting point of 00006

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { LargeAuthorListItems } from "./components/authors/LargeAuthorListItems";
+import { authors } from "./data/authors";
 import "./App.css";
 
 function App() {
-  return <div>where is the list?</div>;
+  const [firstAuthor] = authors;
+
+  return (
+    <div>
+      <button
+        className="bg-blue-400 text-black p-2 rounded-md"
+        onClick={() => console.log(firstAuthor)}
+      >
+        Show Modal
+      </button>
+
+      <LargeAuthorListItems author={firstAuthor} />
+    </div>
+  );
 }
 
 export default App;
