b72020718f245bfde26038347c1c5901da3bfd79
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-06-28 21:30:48 +0300

    00003 add safelist for tailwind grow classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    // Dynamic flex-grow classes
+    'grow-[1]',
+    'grow-[2]'
+  ],
   theme: {
     extend: {},
   },
